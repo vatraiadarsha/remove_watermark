051cc1b50ac64b255c950ee9aefca5444bba19fa
vatraiadarsha <124746349+vatraiadarsha@users.noreply.github.com>
2023-11-04 11:05:48 +0000

    asd

---
+++ app.py
@@ -0,0 +1,23 @@
+import fitz  # PyMuPDF
+
+def remove_watermark(input_pdf, output_pdf, watermark_text):
+    pdf_document = fitz.open(input_pdf)
+    
+    for page_number in range(len(pdf_document)):
+        page = pdf_document[page_number]
+        text_instances = page.search_for(watermark_text)
+        
+        for inst in text_instances:
+            # Remove the watermark text by drawing a white rectangle over it.
+            rect = inst.irect  # Use inst.irect instead of inst.rect
+            page.draw_rect(rect, fill=(1, 1, 1))  # Use draw_rect instead of drawRect
+    
+    pdf_document.save(output_pdf)
+    pdf_document.close()
+
+if __name__ == "__main__":
+    input_pdf_file = "input.pdf"  # Replace with your input PDF file
+    output_pdf_file = "output.pdf"  # Replace with the output PDF file
+    watermark_text_to_remove = "REVIEWER COPY"  # Replace with the watermark text you want to remove
+
+    remove_watermark(input_pdf_file, output_pdf_file, watermark_text_to_remove)
